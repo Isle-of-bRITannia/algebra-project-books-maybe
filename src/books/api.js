43f70b7e0768f50c15e1a49f3646c737c6735125
JohnS-RIT <jts4605@rit.edu>
2023-04-18 19:55:47 -0400

    In-class post-Travis help

--- src/books/api.js
+++ src/books/api.js
@@ -10,6 +10,18 @@ CombineShelves
 EmptyShelf
 */
 
+const Alg = {
+    moveDown: (shelves) => ({
+      _tag: 'move_down',
+      shelves
+    }),
+    newShelf: () => ({
+        _tag: ' new_shelf'
+    })
+}  
+
+//above is for algebra.js
+
 const API = {
   moveDown: (shelves, shelfIndex) => shelves[shelfIndex + 1],
   moveAllTheWayLeft: (shelves, shelfIndex, cursor) => shelves[shelfIndex][cursor] = 0,
@@ -20,6 +32,34 @@ const API = {
   emptyShelf: (shelves, shelfIndex, cursor) => API.downShelf(shelves, shelfIndex, cursor),
   //combineShelves: (shelves, shelfIndex1, shelfIndex2) => shelves[shelfIndex1] = 
 };
+
+//Below is for a separate file observe.js
+interrpet(API.downShelf(API.newShelf()));
+
+cosnt interpret = (shelves) => {
+    let result = {
+        2darray: [],
+        cursor: []
+    }
+    switch (shelves._tag) {
+        case 'move_down':
+   
+            let shelfResult = interpret(shelves.shelves);
+            return {
+                cursor: [cursor[0], cursor[1] + 1],
+                2darray: shelfResult.2darray
+            }
+        case 'new_shelf'
+            return {
+                cursor: [0,0],
+                2darray: [
+                    [],
+                    [],
+                    []
+                ]
+            };
+    }
+}
 
 export {
   API as shelves
